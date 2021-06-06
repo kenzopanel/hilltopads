704d673a0456eda1bc578b72c834b118310ceb98
kenzopanel <65157916+kenzopanel@users.noreply.github.com>
2021-06-06 12:12:47 +0700

    Add files via upload

---
+++ htapop.js
@@ -0,0 +1,8 @@
+(function(__htas){
+var d = document,
+    s = d.createElement('script'),
+    l = d.scripts[d.scripts.length - 1];
+s.settings = __htas || {};
+s.src = "\/\/fagywalu.pro\/cBDi9\/6.bd2p5\/lJSrWHQK9\/NwDzIlz\/NHz-Yt3XMFC\/0z0iMjjxMu3JNzj-cTxF";
+l.parentNode.insertBefore(s, l);
+})({})
---
+++ htapush.js
@@ -0,0 +1,8 @@
+(function(__htas){
+var d = document,
+    s = d.createElement('script'),
+    l = d.scripts[d.scripts.length - 1];
+s.settings = __htas || {};
+s.src = "\/\/fagywalu.pro\/cwHCV.zaaLEk5BvVdOGFl\/mrexUjNyvqZNGWUk\/ie\/mN9juYZEUKlNkNPuTSQSy-Mkz\/ch2\/NpzDgCtONdD\/IYzBN\/zOY\/3WOoSqZMwUdTXbN\/o\/Whmi9HunZBUrlDkSPrTSQtyjMWz\/co2WNrzQgFtTNpDyIXztNpzqYH3SOrQR";
+l.parentNode.insertBefore(s, l);
+})({})
